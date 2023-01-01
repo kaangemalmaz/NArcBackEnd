fc357df982c7f89c5fc753ffdeac5e410a449d73
kaangemalmaz <kaangemalmaz@gmail.com>
2023-01-01 16:39:00 +0300

    Added Cors

--- NArcBackEnd.WebAPI/Program.cs
+++ NArcBackEnd.WebAPI/Program.cs
@@ -16,6 +16,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", //bir isim verme!
+                                     //builder =>builder.WithOrigins("https://localhost:4200") //eriþim olacak yerler verilir. //site bazlý izin.
+        builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()// herþeye izin ver demektir.
+        ); //izin verilme þartlarý
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowOrigin"); //corsa verilen isim verilir. uygulamanýn asýl ayaða kalktýðý yer burasýdýr. uygulamaya izin veriyorsun.
 
 app.UseHttpsRedirection();
 

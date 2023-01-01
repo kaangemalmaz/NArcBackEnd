1043ce577fab43052d07ba0551cf034c48d87be8
kaangemalmaz <kaangemalmaz@gmail.com>
2023-01-01 16:52:47 +0300

    Token Settings - 1

--- NArcBackEnd.WebAPI/Program.cs
+++ NArcBackEnd.WebAPI/Program.cs
@@ -1,6 +1,9 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using NArcBackEnd.Business.DependencyResolvers.Autofac;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +25,22 @@ builder.Services.AddCors(options =>
                                      //builder =>builder.WithOrigins("https://localhost:4200") //eriþim olacak yerler verilir. //site bazlý izin.
         builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()// herþeye izin ver demektir.
         ); //izin verilme þartlarý
+});
+
+//jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true, //tokeni kimin kontrol edip etmeyeceðini bearer kontrol edecek mi?
+        ValidateIssuer = true, //oluþturulacak token deðeri kimden aldý kontrol.
+        ValidateLifetime = false, //token içinde bir experience süresi olacak mý 
+        ValidateIssuerSigningKey = true, // üretilecek token deðerinin uygulamamýza ait olduðunu gösterir.
+        ValidIssuer = builder.Configuration["Token:Issuer"], //oluþturulacak token deðerinin kimin daðýttýðýný gösterir.
+        ValidAudience = builder.Configuration["Token:Audience"], //oluþturalacak token deðerinin hangi sitenin ulaþabileceði
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])), //token deðerinin bize ait olduðunu doðrulayan yapýdýr. yani oluþturankey bize ait odur.
+        ClockSkew = TimeSpan.Zero //expression süresine bu süreyi ekler. server ile bilgisayar arasý farkdan dolayý bu var.
+    };
 });
 
 var app = builder.Build();
